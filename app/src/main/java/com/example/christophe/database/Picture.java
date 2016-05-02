2d510b8a376d3b6c7e17e0736216c5ba7a4e083f
Antoine Rime <rime.antoine@gmail.com>
2016-05-02 12:33:05 +0200

    COmmit Picture

---
+++ app/src/main/java/com/example/christophe/database/Picture.java
@@ -0,0 +1,36 @@
+package com.example.christophe.database;
+
+/*
+ * Created by antoine on 02/05/16.
+ */
+
+public class Picture {
+    public String I_D;
+    public String File;
+    public boolean Profile;
+
+    public Picture (String I_D, String File, boolean Profile){
+
+        this.I_D= I_D;
+        this.File = File;
+        this.Profile = Profile
+    }
+    public String getI_D(){return this.I_D;}
+
+    public void setI_D(String newi){
+        this.I_D = newi;
+    }
+
+    public String getFile(){return this.File;}
+
+    public boolean getProfile(){return this.Profile;}
+
+    public void setProfile (boolean pro){
+        this.Profile = pro;
+    }
+
+
+
+
+
+}
