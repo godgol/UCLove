451309d4151363ddd72c36dda89b4f2ca3652bdd
Christophe <shinnchris@hotmail.com>
2016-05-05 22:29:03 +0200

    next button

--- app/src/main/java/com/example/christophe/uclove/PeopleActivity.java
+++ app/src/main/java/com/example/christophe/uclove/PeopleActivity.java
@@ -5,14 +5,18 @@ import android.database.sqlite.SQLiteQuery;
 import android.os.Bundle;
 import android.provider.Contacts;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Christophe on 05.05.2016.
  */
-public class PeopleActivity extends AppCompatActivity{
+public class PeopleActivity extends AppCompatActivity {
 
     public String login = CurrentUser.current_user;
     //public String login = "Jojelavida";
@@ -24,6 +28,9 @@ public class PeopleActivity extends AppCompatActivity{
     public TextView ey;
     public TextView pre;
     public ImageButton random;
+    ImageButton next;
+    int i = 0;
+    List<String> array = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,6 +48,10 @@ public class PeopleActivity extends AppCompatActivity{
 
         DatabaseHandler2 db = new DatabaseHandler2(PeopleActivity.this);
 
+        array = db.getNonFriendList(login);
+
+        login = array.get(i);
+
         log.setText(login);
         loc.setText(db.readLocation(login));
         gen.setText(db.readGender(login));
@@ -50,5 +61,35 @@ public class PeopleActivity extends AppCompatActivity{
         pre.setText(db.readPreferences(login));
 
         db.closeDB();
+
+        //TODO Button ne marche pas
+        next = (ImageButton) findViewById(R.id.next);
+        next.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    DatabaseHandler2 db2 = new DatabaseHandler2(PeopleActivity.this);
+
+                    log.setText(login);
+                    loc.setText(db2.readLocation(login));
+                    gen.setText(db2.readGender(login));
+                    h.setText(db2.readHair(login));
+                    ag.setText(String.valueOf(db2.readAge(login)));
+                    ey.setText(db2.readEyes(login));
+                    pre.setText(db2.readPreferences(login));
+
+                    db2.closeDB();
+                }
+                else{
+                    i = i + 1;
+                    login = array.get(i);
+                }
+                return true;
+            }
+        });
+
+
+
+
     }
 }
