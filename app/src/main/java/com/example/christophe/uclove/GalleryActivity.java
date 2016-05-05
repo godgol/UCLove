6e19a36c0a022519676b2fa97a0519e342a60876
GillesMottiat <gilles.mottiat@student.uclouvain.be>
2016-05-05 10:36:12 +0200

    g

--- app/src/main/java/com/example/christophe/uclove/GalleryActivity.java
+++ app/src/main/java/com/example/christophe/uclove/GalleryActivity.java
@@ -1,7 +1,86 @@
 package com.example.christophe.uclove;
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
-/**
- * Created by Thomas on 04/05/2016.
- */
-public class GalleryActivity {
+public class GalleryActivity extends Activity {
+    Integer[] pics = {
+            R.drawable.antartica1,
+            R.drawable.antartica2,
+            R.drawable.antartica3,
+            R.drawable.antartica4,
+            R.drawable.antartica5,
+            R.drawable.antartica6,
+            R.drawable.antartica7,
+            R.drawable.antartica8,
+            R.drawable.antartica9,
+            R.drawable.antartica10
+    };
+    ImageView profilePic;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_gallery);
+
+        Gallery gallery = (Gallery)findViewById(R.id.gallery);
+        gallery.setAdapter(new ImageAdapter(this));
+
+        profilePic = (ImageView)findViewById(R.id.ImageView01);
+        gallery.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                Toast.makeText(getBaseContext(),"Define as profile picture",Toast.LENGTH_SHORT).show();
+                imageView.setImageResource(pics[arg2]);
+            }
+        });
+
+    }
+
+
+    public class ImageAdapter extends BaseAdapter {
+
+        private Context ctx;
+        int imageBackground;
+
+        public ImageAdapter(Context c) {
+            ctx = c;
+            TypedArray ta = obtainStyledAttributes(R.styleable.Gallery1);
+            imageBackground = ta.getResourceId(R.styleable.Gallery1_android_galleryItemBackground, 1);
+            ta.recycle();
+        }
+
+        @Override
+        public int getCount() {
+            return pics.length;
+        }
+
+        @Override
+        public Object getItem(int arg0) {
+            return arg0;
+        }
+
+        @Override
+        public long getItemId(int arg0) {
+            return arg0;
+        }
+        @Override
+        public View getView(int arg0, View arg1, ViewGroup arg2) {
+            ImageView iv = new ImageView(ctx);
+            iv.setImageResource(pics[arg0]);
+            iv.setScaleType(ImageView.ScaleType.FIT_XY);
+            iv.setLayoutParams(new Gallery.LayoutParams(150,120));
+            iv.setBackgroundResource(imageBackground);
+            return iv;
+        }
+    }
 }
